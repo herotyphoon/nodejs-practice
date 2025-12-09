93d0797b2f91e011129707bfd06ff465c7d6a43c
HeroTyphoon <pokeaakarsh@gmail.com>
2025-12-09 15:19:57 +0530

    HTTP Headers and Status Codes
    Learning about HTTP Headers and Status Codes

--- project01/index.js
+++ project01/index.js
@@ -31,27 +31,14 @@ app.get('/api/users', (req, res) => {
 
 app.post('/api/users', (req, res) => {
     const body = req.body;
+    const searchValidation = !body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title
+    if (searchValidation) res.status(400).send(JSON.stringify({error:'Please fill all fields'}));
     users.push({id: users.length + 1,...body});
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
-        return res.json({status: 'success', id: users.length});
+        if (err) return res.status(500).json({ status: 'error', message: 'Failed to add user' });
+        return res.status(201).json({status: 'success', id: users.length});
     })
 });
-
-// app.get('/api/users/:id', (req, res) => {
-//     const id = Number(req.params.id);
-//     const user = users.find(user => user.id === id);
-//     return res.json(user);
-// });
-//
-// app.patch('/api/users/:id', (req, res) => {
-//     // later
-//     return res.json({status: 'pending'});
-// });
-//
-// app.delete('/api/users/:id', (req, res) => {
-//     // later
-//     return res.json({status: 'pending'});
-// });
 
 app.route('/api/users/:id')
     .get((req, res) => {
@@ -63,14 +50,12 @@ app.route('/api/users/:id')
     .patch((req, res) => {
         const id = Number(req.params.id);
         const body = req.body;
-
         const user = users.find(u => u.id === id);
         if (!user) return res.status(404).json({ error: "User not found" });
 
         Object.assign(user, body);
         fs.writeFile('./MOCK_DATA.json', JSON.stringify(users, null, 2), (err) => {
             if (err) return res.status(500).json({ status: 'error', message: 'Failed to update user' });
-
             return res.json({ status: 'updated', user });
         });
     })
